861168308bfe55ecddef1a0fc5d762070bbc165c
wilson campusano <wilsoncampusanojorge@gmail.com>
2014-09-07 14:39:42 -0400

    map function

---
+++ map.js
@@ -0,0 +1,10 @@
+//actualy dont know what the heck is this
+function curve(grade){
+	return grade += 5;
+}
+
+
+
+var grades = [1,2,3,4,5];
+var newgrades = grades.map(curve);
+console.log(newgrades);
